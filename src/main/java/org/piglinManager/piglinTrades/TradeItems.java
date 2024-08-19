5eadc78e4b16f559b00f02b65799db09a31b01bb
Hagymasi Levente <65444028+Levy-Y@users.noreply.github.com>
2024-08-19 23:17:03 +0200

    Alpha v1.0 features: - Vanilla, Oraxen item support - Item trade chance - Item quantity

---
+++ src/main/java/org/piglinManager/piglinTrades/TradeItems.java
@@ -0,0 +1,32 @@
+package org.piglinManager.piglinTrades;
+
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class TradeItems {
+    private final Random random = new Random();
+
+    public HashMap<ItemStack, Integer> items = new HashMap<>();
+
+    public static ItemStack selectItem(HashMap<ItemStack, Integer> items) {
+        int totalWeight = 0;
+        for (int weight : items.values()) {
+            totalWeight += weight;
+        }
+
+        Random random = new Random();
+        int randomWeight = random.nextInt(totalWeight);
+
+        for (Map.Entry<ItemStack, Integer> entry : items.entrySet()) {
+            randomWeight -= entry.getValue();
+            if (randomWeight < 0) {
+                return entry.getKey();
+            }
+        }
+
+        return null;
+    }
+}
